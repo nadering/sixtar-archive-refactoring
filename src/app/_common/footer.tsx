cf598e778e04902b18d864dd1758dc933a613952
nadering <120453768+nadering@users.noreply.github.com>
2024-10-14 21:23:42 +0900

    Design: Footer 문구 수정
    Footer 문구의 "식스타 게이트: 스타트레일" 부분에 하이퍼링크 추가 및 큰따옴표 제거

--- src/app/_common/footer.tsx
+++ src/app/_common/footer.tsx
@@ -1,14 +1,18 @@
 export default function Footer() {
   return (
-    <footer
-      className="flex flex-col w-full bg-gray-100 border-t-2 border-gray-500 mt-2 px-5 py-4 gap-y-3"
-    >
+    <footer className="flex flex-col w-full bg-gray-100 border-t-2 border-gray-500 mt-2 px-5 py-4 gap-y-3">
       <p className="text-black text-base font-normal">
         본 사이트는 게임{" "}
-        <span className="font-semibold">"식스타 게이트: 스타트레일"</span>의 팬
-        사이트이며, {" "}
-        <br className="hidden sm:inline" />
-        사이트 내 사용된 모든 게임 컨텐츠의 저작권은 {" "}
+        <a
+          className="font-semibold underline underline-offset-2"
+          href="https://store.steampowered.com/app/1802720/Sixtar_Gate_STARTRAIL/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          식스타 게이트: 스타트레일
+        </a>
+        의 팬 사이트이며, <br className="hidden sm:inline" />
+        사이트 내 사용된 모든 게임 컨텐츠의 저작권은{" "}
         <span className="font-semibold">STARLIKE Inc.</span>에 있습니다.
       </p>
       <p className="text-black text-base font-normal">
