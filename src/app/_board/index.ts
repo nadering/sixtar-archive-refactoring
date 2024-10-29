84e9b37df56a99c13d791d2e08215028e34f91b1
nadering <120453768+nadering@users.noreply.github.com>
2024-10-29 22:54:47 +0900

    Fix: Board에 index 추가
    - Board에 index가 없어 인식되지 않던 현상 수정

---
+++ src/app/_board/index.ts
@@ -0,0 +1,15 @@
+import HomeInformation from "./home-information";
+import MainBoard from "./main-board";
+import PatternBoardDifficultyPart from "./pattern-board-difficutly-part";
+import PatternBoardFloorPart from "./pattern-board-floor-part";
+import PatternBoardPatternPart from "./pattern-board-pattern-part";
+import PatternBoard from "./pattern-board";
+
+export {
+  HomeInformation,
+  MainBoard,
+  PatternBoardDifficultyPart,
+  PatternBoardFloorPart,
+  PatternBoardPatternPart,
+  PatternBoard,
+};
